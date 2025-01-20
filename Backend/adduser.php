18575a725a88a14d571fc155e77cee8a1df1a0d7
linus0508 <91020094+linus0508@users.noreply.github.com>
2025-01-20 11:54:48 +0100

    Added Login functionality

---
+++ Backend/adduser.php
@@ -0,0 +1,26 @@
+<?php 
+include 'connect.php';
+ 
+if (isset($_POST['username'])) {
+    $username_input = $_POST['username'];
+    $firstname_input = $_POST['firstname'];
+    $lastname_input = $_POST['lastname'];
+    $passwort_input = $_POST['passwort'];
+
+    $password_hashed = password_hash($passwort_input, PASSWORD_DEFAULT);
+    
+    $add_user = "INSERT INTO user (username, firstname, lastname, password) VALUES ('$username_input', '$firstname_input', '$lastname_input', '$password_hashed')";
+    $result = mysqli_query($conn, $add_user);
+
+    if (mysqli_query($conn, $add_user)) {
+        echo "User hinzugefügt.";
+    } else {
+        echo "Error in Database Entry";
+    }
+
+    mysqli_close($conn);
+         
+}
+else {echo "General Error";}
+
+?>
--- Backend/login.php
+++ Backend/login.php
@@ -1,24 +1,24 @@
 <?php 
 include 'connect.php';
  
-if(isset($_GET['user'])) {
-    $user_input = $_POST['user'];
+if (isset($_POST['username'])) {
+    $user_input = $_POST['username'];
     $passwort_input = $_POST['passwort'];
     
-    $check_user = "SELECT * FROM user WHERE user = '$user_input'";
+    $check_user = "SELECT * FROM user WHERE username = '$user_input'";
     $result = mysqli_query($conn, $check_user);
         
     if (mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
             $hashed_password = $row["password"];
 
-            if(password_verify($$passwort_input, $hashed_password)) {
+            if(password_verify($passwort_input, $hashed_password)) {
                 echo "Logged In!";
             } 
         }
         } 
         else {
-            echo "Error!";
+            echo "Error: User does not exist!";
         }
     
 }
